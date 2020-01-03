07118dbc81172bdd54b134a5e79bf8521de37e77
Max <tihonov.m.i@gmail.com>
2020-01-03 23:01:55 +0700

    Initialize game field

--- app/src/main/java/justcompany/noughtsandcrosses/MainActivity.java
+++ app/src/main/java/justcompany/noughtsandcrosses/MainActivity.java
@@ -6,6 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TicTacToe field = new TicTacToe(
+            Arrays.asList(
+            R.id.button11, R.id.button12, R.id.button13,
+            R.id.button21, R.id.button22, R.id.button23,
+            R.id.button31, R.id.button32, R.id.button33
+    ));
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
