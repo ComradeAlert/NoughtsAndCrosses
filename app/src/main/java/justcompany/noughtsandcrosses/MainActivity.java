cca8f31e85bfa393562c77c3fb0a6b0880b8c964
Max <tihonov.m.i@gmail.com>
2020-01-04 07:50:58 +0700

    Add message about mistake

--- app/src/main/java/justcompany/noughtsandcrosses/MainActivity.java
+++ app/src/main/java/justcompany/noughtsandcrosses/MainActivity.java
@@ -1,5 +1,7 @@
 package justcompany.noughtsandcrosses;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -32,9 +34,24 @@ public class MainActivity extends AppCompatActivity {
             field.setStatus(btn.getId(), Status.CROSS);
         }
 
-        if (status.equals(Status.CROSS)){
-            btn.setText("O");
-            field.setStatus(btn.getId(), Status.NOUGHT);
+        if (status.equals(Status.CROSS) || status.equals(Status.NOUGHT)){
+            msgMistake("Так нельзя =)");
         }
+    }
+
+    private void msgMistake(String text) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setTitle("Sorry. You are mistaken.");
+        alertDialogBuilder
+                .setMessage(text)
+                .setCancelable(true)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
     }
 }
